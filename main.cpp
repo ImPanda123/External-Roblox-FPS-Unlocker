cb9c82af2a5c846e8a92a1236a0bbee86f73cbf9
Viliyan Yordanov <42891941+ImPanda123@users.noreply.github.com>
2020-04-14 11:25:59 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,62 @@
+#include <Windows.h>
+#include <iostream>
+#include <vector>
+#include <TlHelp32.h>
+#include <tchar.h>
+
+using namespace std;
+
+
+DWORD dwGetModuleBaseAddress(TCHAR* lpszModuleName, DWORD pID) {
+	DWORD dwModuleBaseAddress = 0;
+	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, pID);
+	MODULEENTRY32 ModuleEntry32 = { 0 };
+	ModuleEntry32.dwSize = sizeof(MODULEENTRY32);
+
+	if (Module32First(hSnapshot, &ModuleEntry32))
+	{
+		do {
+			if (_tcscmp(ModuleEntry32.szModule, lpszModuleName) == 0)
+			{
+				dwModuleBaseAddress = (DWORD)ModuleEntry32.modBaseAddr;
+				break;
+			}
+		} while (Module32Next(hSnapshot, &ModuleEntry32));
+
+
+	}
+	CloseHandle(hSnapshot);
+	return dwModuleBaseAddress;
+}
+
+
+int main() {
+	DWORD pID;
+	DWORD baseAddress;
+	DWORD AdressValue;
+	DOUBLE NewAdressValue =0.000006;
+	char moduleName[] = "RobloxPlayerBeta.exe";
+	HWND hGameWindow;
+	HANDLE pHandle;
+	cout << "External FPS Unlocker" <<endl;
+
+	bool exit = false;
+
+	while (exit == false)
+	{
+
+		{
+
+			hGameWindow = FindWindow(NULL, "Roblox");
+			GetWindowThreadProcessId(hGameWindow, &pID);
+			pHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pID);
+			DWORD clientBase = dwGetModuleBaseAddress(_T(moduleName), pID);
+			ReadProcessMemory(pHandle, (LPCVOID)(clientBase + 0x01EB0910), &baseAddress, sizeof(baseAddress), NULL);
+			AdressValue = baseAddress + 0x128;
+
+			WriteProcessMemory(pHandle, (LPVOID)(AdressValue), &NewAdressValue, sizeof(NewAdressValue), 0);
+
+			Sleep(1000);
+		}
+	}
+}
